09802ba569aaa90846ed434aae8de7c8ae6c4524
vermav27 <verma.vineet27@gmail.com>
2025-09-15 10:08:34 +0530

    java basics 3

---
+++ src/ex_01_JavaBasics/Lab012_Variables.java
@@ -0,0 +1,18 @@
+package ex_01_JavaBasics;
+
+public class Lab012_Variables {
+    public static void main(String[] args) {
+        //variable are like
+        //VariableType VariableName = VariableValue
+        //Example
+
+        int age = 35;
+        String Name = "Vineet";
+
+        //here a is primitive type of datatype. which can be distributed
+        int a = 35;
+        a = 20+50;
+        System.out.println("Value of a : " + a);
+
+    }
+}
---
+++ src/ex_01_JavaBasics/Lab014_JavaVariableNames.java
@@ -0,0 +1,9 @@
+package ex_01_JavaBasics;
+
+public class Lab014_JavaVariableNames {
+    public static void main(String[] args) {
+        int _ = 65;
+        int $ = 100;
+        //so according to the rules _ & $ can also be a variable name.
+    }
+}
