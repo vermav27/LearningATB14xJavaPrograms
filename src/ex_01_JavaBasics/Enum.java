14fbfa7cf48099b8f538ddbd14dd788ac660fd1d
vermav27 <verma.vineet27@gmail.com>
2025-09-15 09:13:06 +0530

    Enaum

--- src/ex_01_JavaBasics/Enum.java
+++ src/ex_01_JavaBasics/Enum.java
@@ -1,4 +1,9 @@
 package ex_01_JavaBasics;
 
 public class Enum {
+
+    //class name can be ENUM as well. as enum is a keyword and Enum is a normal word.
+    // And java is case sensitive.
+    //So we can use Enum as identifier.
+
 }
