c642d3bc2a53eaf78f9f644da9a142dd0b7df491
vermav27 <verma.vineet27@gmail.com>
2025-09-16 09:43:20 +0530

    Table of 9 task

---
+++ src/ex_03_Operators/Lab029_TSK_TableOf9.java
@@ -0,0 +1,22 @@
+package ex_03_Operators;
+
+public class Lab029_TSK_TableOf9 {
+    public static void main(String[] args) {
+        /*Print like :
+        9×1 = 9
+        9×2 = 18
+        9×3 = 27*/
+
+        int num = 9;
+        System.out.printf("%d x 1 = %d \n",num,num*1);
+        System.out.printf("%d x 2 = %d \n",num,num*2);
+        System.out.printf("%d x 3 = %d \n",num,num*3);
+        System.out.printf("%d x 4 = %d \n",num,num*4);
+        System.out.printf("%d x 5 = %d \n",num,num*5);
+        System.out.printf("%d x 6 = %d \n",num,num*6);
+        System.out.printf("%d x 7 = %d \n",num,num*7);
+        System.out.printf("%d x 8 = %d \n",num,num*8);
+        System.out.printf("%d x 9 = %d \n",num,num*9);
+        System.out.printf("%d x 10 = %d \n",num,num*10);
+    }
+}
