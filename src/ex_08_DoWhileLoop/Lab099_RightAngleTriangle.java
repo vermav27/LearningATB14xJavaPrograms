b39dc9f29c915b54b4084780864771e03f3fd6c8
vermav27 <verma.vineet27@gmail.com>
2025-09-26 10:27:51 +0530

    triangel printing

---
+++ src/ex_08_DoWhileLoop/Lab099_RightAngleTriangle.java
@@ -0,0 +1,23 @@
+package ex_08_DoWhileLoop;
+
+public class Lab099_RightAngleTriangle {
+    public static void main(String[] args) {
+        /* Print a right angle triangle
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
+        */
+        for(int i = 1 ; i <= 5 ; i++){
+
+            for (int j = 1 ; j <= i ;j++){
+
+                System.out.printf("*");
+
+            }
+            System.out.printf("\n");
+        }
+
+    }
+}
