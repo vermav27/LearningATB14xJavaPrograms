54ee3d425bebb712ee987e6362edbc4f0432bddc
vermav27 <verma.vineet27@gmail.com>
2025-09-28 09:47:37 +0530

    functions

---
+++ src/ex_09_Functions/Lab103_Declaration_Calling.java
@@ -0,0 +1,23 @@
+package ex_09_Functions;
+
+public class Lab103_Declaration_Calling {
+
+    //format :
+    /*
+
+    This is how a function is created :
+    public returnType functionName ( patameterType parameterName ){
+        body of the function
+     }
+
+     Calling Function :
+     functionName(arguments);
+
+     */
+
+    //main is also a function
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
