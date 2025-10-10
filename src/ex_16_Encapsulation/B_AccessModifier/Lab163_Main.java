1ea2df7a8bb0003dff6fb51813e75980ab39f2a8
vermav27 <verma.vineet27@gmail.com>
2025-10-10 13:08:56 +0530

    Super Keyword

--- src/ex_16_Encapsulation/B_AccessModifier/Lab163_Main.java
+++ src/ex_16_Encapsulation/B_AccessModifier/Lab163_Main.java
@@ -5,6 +5,6 @@ import ex_16_Encapsulation.B_AccessModifier.A_Police.Lab160_cop;
 public class Lab163_Main {
     public static void main(String[] args) {
         Lab160_cop c1 = new Lab160_cop(10);
-        c1.canIShoot();
+        //c1.canIShoot();
     }
 }
