8e660341574582b061b2b21f282e18e80ff6ce56
vermav27 <verma.vineet27@gmail.com>
2025-10-12 09:51:27 +0530

    abstraction start

---
+++ src/ex_18_Abstraction/Lab165_Abstrcation.java
@@ -0,0 +1,13 @@
+package ex_18_Abstraction;
+
+public class Lab165_Abstrcation {
+    public static void main(String[] args) {
+
+        /*
+        * Abstraction is a java concept that hides the complex details and shows only essential features.
+        * It can be achieved through the abstract classes and interfaces.
+        * Abstract means : INCOMPLETE
+        * */
+
+    }
+}
